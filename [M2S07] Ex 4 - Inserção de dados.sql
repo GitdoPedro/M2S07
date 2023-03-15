c47d0aac5d6ae9d4e66858c112e391516159a656
GitdoPedro <emailpedrolima@gmail.com>
2023-03-15 16:35:33 -0300

    [M2S07] Ex 4 - Inserção de dados

---
+++ [M2S07] Ex 4 - Inserção de dados.sql
@@ -0,0 +1,13 @@
+INSERT INTO cursos (nome,modalidade,id_professores,quantidadealunos)
+values 
+    ('Filosofia','presencial',2,10);
+
+
+INSERT INTO ESTUDANTES (nome,modalidade,id_professores,quantidadealunos)
+values 
+    ('Lucas'	,'pedrosa',	'2032123132',	'veve@gmail.com',	'15/05/1990',	'brasileiro',	7,8)
+
+
+INSERT INTO PROFESSORES (nome,modalidade,id_professores,quantidadealunos)
+values 
+    ('Pedro',	'Lima'	,'2032123132'	,'emailpedrolima@gmail.com',	'15/05/1990','brasileiro');
